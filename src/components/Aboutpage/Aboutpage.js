f8a4206d0e1431cbaa3640ba65ee48f039608a77
cynthiaaguero <cynthiaaguero02@gmail.com>
2023-04-21 19:33:19 -0400

    aboutpage??

--- src/components/Aboutpage/Aboutpage.js
+++ src/components/Aboutpage/Aboutpage.js
@@ -10,22 +10,25 @@ function Aboutpage() {
                 <Row className='textbackground'>
                     <Col md={7} >
                         <h3 className='aboutmetext'>About <span>Me</span></h3>
-                        <p className='aboutdetails'>I'm a self-taught Front End developer from India. I build websites with a focus on providing the experience for everyone using them and responsiveness. Curious to learn more about developing scalable distribution systems, love problem solving, and care about writing and maintainable code. A responsive design makes your website accessible to all users, regardless of their devices.
+                        <p className='aboutdetails'> I am a dedicated Computer Science major studying at Rutgers 
+                        University. I am currently interested in exploring Full Stack Development, Artificial Intelligence, 
+                        Machine Learning, and Data Science. Some relevant courses I have taken so far are Data Structures,
+                        Computer Architecture, and Discrete Structures. In my leisure time, I pursue interests such as
+                        studying languages, rock climbing, reading, and running. Some languages I am currently studying
+                        are Japanese and Spanish.
                         </p>
                         <ul className='skilllist'>
                             <Row>
                                 <h3>Skills</h3>
                                 <Col md={7}>
-                                    <li>HTML5/CSS3</li>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
                                     <li>JavaScript</li>
-                                    <li>jQuery</li>
-                                    <li>Bootsrap 5</li>
+                                    <li>Java</li>
                                 </Col>
                                 <Col md={5}>
-                                    <li>React Js</li>
-                                    <li>Redux Js</li>
-                                    <li>React-Bootsrap</li>
-                                    <li>Material-ui</li>
+                                    <li>C</li>
+                                    <li>Tableu</li>
                                     <li>Git/Github</li>
                                 </Col>
                             </Row>
