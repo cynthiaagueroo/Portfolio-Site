1a7ac67414e00083e34300dedbd890832a3e0643
cynthiaaguero <cynthiaaguero02@gmail.com>
2023-04-21 20:35:42 -0400

    fix smile

--- src/components/Contactpage/Contactpage.js
+++ src/components/Contactpage/Contactpage.js
@@ -8,7 +8,7 @@ function Contactpage() {
       <Container>
         <h2 className='contacthead'>Contact me</h2>
         <p className='contactpara'> I’m currently searching for internship opportunities. 
-          <br /> If you would like to be in touch, my inbox is always open! ':]'
+          <br /> If you would like to be in touch, my inbox is always open! :]
         </p>
         <button
           className='contactbtn'
